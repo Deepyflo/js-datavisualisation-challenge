aaa3da7d7c83a80aed347e62412fd63b583be62e
Florian Renders <florianrenders@gmail.com>
2021-10-28 09:28:18 +0200

    repair

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,8 @@ table.insertAdjacentHTML('beforebegin', '<canvas id="cvs2" width="400" height="2
 let h1 = document.getElementById("firstHeading");
 h1.insertAdjacentHTML('afterend', '<canvas id="cvsA" width="400" height="200">Canvas not supported on tour browser...</canvas>');
 
-let ctx = document.getElementById('cvs2');
-let myChart = new Chart(ctx, {
+let ctxB = document.getElementById('cvs2');
+let myChart = new Chart(ctxB, {
     type: 'bar',
     data: {
         labels: ['2007-09', '2010-12'],
@@ -111,3 +111,119 @@ let chartApi = async () => {
     // animChart.update();
 }
 chartApi();
+
+
+
+/*==========| setup var|========== */
+const table1        = document.getElementById('table1');
+const reg           = new RegExp( /(\(.\))/g );
+let dataObj         = {};
+const tableOneDatas = [];
+
+const canvas = document.createElement('canvas');
+canvas.id = 'chartTableOne';
+const divVoisin = document.getElementById('toc');
+divVoisin.parentNode.insertBefore(canvas, divVoisin.nextSibling);
+
+
+/*====================================================================================== */
+/*=============================| récupération des donnés |================================ */
+
+/* Je récupère chaque row du tableau 1 que je place dans un array inside un array*/
+// help source: https://stackoverflow.com/a/68047870
+
+const tableOneDatasBrut = [...table1.rows].map(row => [...row.cells].map(td => td.innerText));
+tableOneDatasBrut.splice(0,2); /* supprime les 2 prmières row*/
+
+
+/* surprime les caractères spéciaux: (²) */
+tableOneDatasBrut.forEach(element => {
+     element[1] = element[1].replace(reg, '');
+})
+
+
+/* Je récupère la row du tableau avec les dates et remplace les deux 1er éléments par id et country*/
+let dataKeyTable1 = [...table1.rows[1].cells].map(el => el.innerText);
+dataKeyTable1.splice(0,2, 'id','country');
+
+
+/* création d'un tableau contenant chaque pays et leurs données en tant qu'objet */
+tableOneDatasBrut.forEach(elem => {
+    result =  Object.assign.apply({}, dataKeyTable1.map( (key, value) => ( {[key]: elem[value]} ) ) );
+    tableOneDatas.push(result);
+});
+// help source: https://www.codegrepper.com/code-examples/javascript/javascript+combine+two+arrays+to+object
+
+
+
+/*====================================================================================== */
+/*=============================| Chart table 1 |================================ */
+
+/*==========| setup var chart|========== */
+let ctxA      = document.getElementById('chartTableOne').getContext('2d');
+const labels = tableOneDatas.map(elem => elem.country);
+
+/*==========| setup colors|========== */
+    const clrOne   = '255, 99, 132';
+    const clrTwo   = '54, 162, 235';
+    const clrThree = '255, 206, 86';
+    const clrFour  = '75, 192, 192';
+    const clrFive  = '153, 102, 255';
+    const clrSix   = '255, 159, 64';
+
+    const clrBgOpacity     = 0.2;
+    const clrBorderOpacity = 1;
+
+    const colors = [clrOne, clrTwo, clrThree, clrFour, clrFive, clrSix];
+    const randomClr = () => colors[ Math.floor(Math.random() * colors.length)];
+
+/*=============================| Le Chart table 1 |================================ */
+
+let chartTableOne = new Chart(ctxA, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: datasetsTable1(),
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Crimes recorded by the police'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            },
+            x: {
+                ticks: {
+                    autoSkip: false,
+                    maxRotation: 90,
+                    minRotation: 90
+                }
+            }
+        }
+    }
+});
+
+/*=============================| Function pour intégrer les datas |================================ */
+
+function datasetsTable1 () {
+    let datasets = [];
+    for(i=2; i < dataKeyTable1.length; i++) {
+        obj = { 
+                label: dataKeyTable1[i],
+                data: tableOneDatas.map(elem => parseInt(elem[dataKeyTable1[i]])),
+                backgroundColor: `rgba(${randomClr()}, ${clrBgOpacity})`,
+                borderColor:     `rgba(${randomClr()}, ${clrBorderOpacity})`,
+                borderWidth: 1,
+        }
+        datasets.push(obj);
+    } 
+    return datasets;
+}
